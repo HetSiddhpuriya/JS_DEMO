da25f6c4f751e8de87331c8f669812d8ca198e6d
HetSiddhpuriya <het.siddhpuriya123@gmail.com>
2026-02-14 16:36:44 +0530

    Event Functions

---
+++ event.js
@@ -0,0 +1,53 @@
+// Event – page par koi pan action karo tene ek event rise karshe,
+// page par click, hover, typing all are events
+// Event Listener – event nu reaction -- give reaction when click,
+// dblclick, hover, typing
+
+// Add Event Listener
+// select --> event --> function --> what you change
+// element.addEventListener("event_name", function(){})
+
+// ===============================================
+// Common Events : click, input, change, submit, mouseover, keyup
+// Click
+// let btn = document.querySelector("button");
+// console.dir(btn);
+
+// btn.addEventListener("click", function(){
+//     document.body.classList.toggle("bg-body")
+// });
+
+
+// 1. Theme Toggle Logic
+let themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("bg-body");
+});
+
+// 2. File Upload Name on Button Logic
+let fileInput = document.querySelector("#file-upload");
+let buttonLabel = document.querySelector("#button-text");
+
+fileInput.addEventListener("change", function(event) {
+    
+    if (event.target.files.length > 0) {
+       
+        let fileName = event.target.files[0].name;
+        buttonLabel.innerText = fileName;
+        console.log("File uploaded:", fileName);
+    } else {
+        buttonLabel.innerText = "Upload File";
+    }
+});
+
+// 1. Select the elements
+let colorPicker = document.querySelector("#color-picker");
+let bigBox = document.querySelector("#big-box");
+
+// 2. Add the Event Listener
+colorPicker.addEventListener("input", function(event) {
+    
+    let selectedColor = event.target.value;
+    console.log("Selected Color:", selectedColor);
+    bigBox.style.backgroundColor = selectedColor;
+});
